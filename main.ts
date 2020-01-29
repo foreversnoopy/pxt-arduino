7c1d23abe94713f5519c968a16d2fe11d4ff68ba
foreversnoopy <maxsnoop@hotmail.com>
2020-01-28 20:28:47 -0500

    Export class instead of preconfigured instance and add constructor with iicAddress parameter

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 namespace sensors {
-    class ArduinoIICSensor extends sensors.internal.IICSensor {
+    export class ArduinoIICSensor extends sensors.internal.IICSensor {
+
+        iicAddress: number
+
+        constructor(port: number, iicAddress: number) {
+            super(port)
+            this.iicAddress = iicAddress
+        }
 
         getDHT() {
-            this.transaction(16, [], 8)
+            this.transaction(this.iicAddress, [], 8)
             const bytes = this.getBytes()
             const h: number = bytes.getNumber(NumberFormat.Float32LE, 0)
             const t: number = bytes.getNumber(NumberFormat.Float32LE, 4)
@@ -17,6 +24,4 @@ namespace sensors {
             return 'ArduinoIIC'
         }
     }
-
-    export const arduino1 = new ArduinoIICSensor(1)
 }
