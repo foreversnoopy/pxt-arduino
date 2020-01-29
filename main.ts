334b008ea4c0ecee6a88eb5d1b0e90085b10805b
foreversnoopy <maxsnoop@hotmail.com>
2020-01-28 19:36:06 -0500

    First implementation of ArduinoIICSensor with a method to get two floating point values over IIC.

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
-forever(function () {
-	
-})
+namespace sensors {
+    class ArduinoIICSensor extends sensors.internal.IICSensor {
+
+        getDHT() {
+            this.transaction(16, [], 8)
+            const bytes = this.getBytes()
+            const h: number = bytes.getNumber(NumberFormat.Float32LE, 0)
+            const t: number = bytes.getNumber(NumberFormat.Float32LE, 4)
+            return [h, t]
+        }
+
+        getAll() {
+            return this.getDHT()
+        }
+
+        _IICId() {
+            return 'ArduinoIIC'
+        }
+    }
+
+    //export const arduino1 = new ArduinoIICSensor(1)
+}
